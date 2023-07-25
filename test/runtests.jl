581eb8046fdbca7539bceec5f6a4835d3ea47791
Lance (Weiqing) Xu <lance.xwq@gmail.com>
2023-07-25 10:41:01 -0700

    use GPU to calculate contributions from emitters

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,13 +7,13 @@ using Test
 
 params = SpBNPTrack.ExperimentalParameters()
 priors = SpBNPTrack.Priors(
-    location_μx = params.pixelnumx * params.pixelsize / 2,
-    location_μy = params.pixelnumy * params.pixelsize / 2,
-    location_σx = params.pixelsize * 2,
-    location_σy = params.pixelsize * 2,
+    location_μx = params.pxnumx * params.pxsize / 2,
+    location_μy = params.pxnumy * params.pxsize / 2,
+    location_σx = params.pxsize * 2,
+    location_σy = params.pxsize * 2,
 )
 
-(video, gt) = SpBNPTrack.forward_main(params, priors, emission = 40.0, background = 10.0)
+(video, gt) = SpBNPTrack.forward_main(params, priors, emission = 200.0, background = 10.0)
 
 fig = SpBNPTrack.visualize_data_3D(video, gt)
 
