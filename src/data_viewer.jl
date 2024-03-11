1a7f278b8ff10281955481d7377403d9d342ba7a
Lance (Weiqing) Xu <lance.xwq@gmail.com>
2024-03-11 13:19:31 -0700

    use @lift to make code concise

--- src/data_viewer.jl
+++ src/data_viewer.jl
@@ -48,49 +48,28 @@ function view_frames(
             format = x -> "$x/$framecount",
         ),
     )
-
     textbox = Textbox(
         fig[2, 3][1, 2],
         placeholder = "start frame, end frame",
         validator = range_validator,
     )
-    startframeindex = Observable(1)
-    endframeindex = Observable(framecount)
+    lowerindex = Observable(1)
+    upperindex = Observable(framecount)
 
     on(textbox.stored_string) do str
         interval = sort!(parse.(Int64, split(str, ',')))
-        startframeindex[] =
-            ifelse(1 <= interval[1] <= framecount, interval[1], startframeindex[])
-        endframeindex[] =
-            ifelse(1 <= interval[2] <= framecount, interval[2], endframeindex[])
+        lowerindex[] = ifelse(1 <= interval[1] <= framecount, interval[1], lowerindex[])
+        upperindex[] = ifelse(1 <= interval[2] <= framecount, interval[2], upperindex[])
     end
 
-    bottomlefttext = lift(widthslider.interval, heightslider.interval) do w_int, h_int
-        w, h = w_int[1], h_int[1]
-        "($w, $h)"
-    end
+    bottomlefttag = @lift "($($(widthslider.interval)[1]), $($(heightslider.interval)[1]))"
+    toprighttag = @lift "($($(widthslider.interval)[2]), $($(heightslider.interval)[2]))"
+    rangetag =
+        @lift "Frame range: $(min($lowerindex,$upperindex))-$(max($lowerindex,$upperindex))"
 
-    toprighttext = lift(widthslider.interval, heightslider.interval) do w_int, h_int
-        w, h = w_int[2], h_int[2]
-        "($w, $h)"
-    end
-
-    framerangetext = lift(startframeindex, endframeindex) do val1, val2
-        minval, maxval = minmax(val1, val2)
-        "Frame range: $minval-$maxval"
-    end
-
-    mainframe = lift(frameslider.sliders[1].value) do val
-        view(frames, :, :, val)
-    end
-
-    startframe = lift(startframeindex) do val
-        view(frames, :, :, val)
-    end
-
-    endframe = lift(endframeindex) do val
-        view(frames, :, :, val)
-    end
+    mainframe = @lift view(frames, :, :, $(frameslider.sliders[1].value))
+    startframe = @lift view(frames, :, :, $lowerindex)
+    endframe = @lift view(frames, :, :, $upperindex)
 
     # color points differently if they are within the two intervals
 
@@ -101,37 +80,21 @@ function view_frames(
     #     @. (h_int[1] <= pointsx <= h_int[2]) & (v_int[1] <= pointsy <= v_int[2])
     # end
 
-    lbound = lift(widthslider.interval) do w_int
-        w_int[1] - 0.5
-    end
-    rbound = lift(widthslider.interval) do w_int
-        w_int[2] + 0.5
-    end
-    bbound = lift(heightslider.interval) do h_int
-        h_int[1] - 0.5
-    end
-    tbound = lift(heightslider.interval) do h_int
-        h_int[2] + 0.5
-    end
+    lbound = @lift $(widthslider.interval)[1] - 0.5
+    rbound = @lift $(widthslider.interval)[2] + 0.5
+    bbound = @lift $(heightslider.interval)[1] - 0.5
+    tbound = @lift $(heightslider.interval)[2] + 0.5
 
-    xmin = lift(widthslider.interval) do w_int
-        (w_int[1] - 1) / framewidth
-    end
-    xmax = lift(widthslider.interval) do w_int
-        w_int[2] / framewidth
-    end
-    ymin = lift(heightslider.interval) do h_int
-        (h_int[1] - 1) / frameheight
-    end
-    ymax = lift(heightslider.interval) do h_int
-        h_int[2] / frameheight
-    end
+    xmin = @lift ($(widthslider.interval)[1] - 1) / framewidth
+    xmax = @lift $(widthslider.interval)[2] / framewidth
+    ymin = @lift ($(heightslider.interval)[1] - 1) / framewidth
+    ymax = @lift $(heightslider.interval)[2] / framewidth
 
     heatmap!(axes[1], 1:framewidth, 1:frameheight, mainframe, colormap = :bone)
     heatmap!(axes[2], 1:framewidth, 1:frameheight, startframe, colormap = :bone)
     heatmap!(axes[3], 1:framewidth, 1:frameheight, endframe, colormap = :bone)
 
-    limits!.(axes[1:3], 0.5, framewidth + 0.5, 0.5, frameheight + 0.5)
+    limits!.(axes, 0.5, framewidth + 0.5, 0.5, frameheight + 0.5)
 
     for ax in axes
         vlines!(ax, lbound, ymin = ymin, ymax = ymax, color = ColorSchemes.tab10[1])
@@ -140,12 +103,12 @@ function view_frames(
         hlines!(ax, tbound, xmin = xmin, xmax = xmax, color = ColorSchemes.tab10[1])
     end
 
-    text!(axes[1], lbound, bbound, align = (:left, :bottom), text = bottomlefttext)
-    text!(axes[1], rbound, tbound, align = (:right, :top), text = toprighttext)
+    text!(axes[1], lbound, bbound, align = (:left, :bottom), text = bottomlefttag)
+    text!(axes[1], rbound, tbound, align = (:right, :top), text = toprighttag)
 
     Label(fig[0, 1:2], "Frame viewer")
     Label(fig[0, 3], "Range viewer")
-    Label(fig[2, 3][1, 1], framerangetext, justification = :left)
+    Label(fig[2, 3][1, 1], rangetag, justification = :left)
 
     colsize!(fig.layout, 1, Relative(2 / 3))
     colsize!(fig.layout, 3, Relative(1 / 3))
