c2eadc113c4dbb143db170f82a7f4d8ab299ed8b
Lance (Weiqing) Xu <47257262+lanceXwq@users.noreply.github.com>
2024-03-22 15:18:51 -0700

    use observables of Rect and Point

--- src/data_viewer.jl
+++ src/data_viewer.jl
@@ -12,6 +12,7 @@ theme_data_viewer = Theme(
         yticklabelsvisible = false,
         yticksvisible = false,
     ),
+    Poly = (strokecolor = ColorSchemes.tab10[1], strokewidth = 2),
 )
 
 function range_validator(s::String)
@@ -19,37 +20,39 @@ function range_validator(s::String)
     return eltype(parsed_vec) == Int64 && length(parsed_vec) == 2
 end
 
+getrect(w_sl, h_sl) = @lift Rect(
+    $(w_sl.interval)[1] - 0.5,
+    $(h_sl.interval)[1] - 0.5,
+    $(w_sl.interval)[2] - $(w_sl.interval)[1] + 1,
+    $(h_sl.interval)[2] - $(h_sl.interval)[1] + 1,
+)
+
+getvertices(rect) = (@lift $(rect).origin), (@lift $(rect).origin .+ $(rect).widths)
+
 function view_frames(
     frames::AbstractArray{<:Integer};
     max_intensity::Union{Integer,Nothing} = nothing,
 )
     isnothing(max_intensity) && (max_intensity = maximum(frames))
-    framewidth, frameheight, framecount = size(frames)
-    widthrange, heightrange = 1:framewidth, 1:frameheight
+    w, h, c = size(frames)
 
     set_theme!(theme_data_viewer)
     fig = Figure(; size = (1000, 300))
     axes = [Axis(fig[1, 1]), Axis(fig[1, 3][1, 1]), Axis(fig[1, 3][2, 1])]
 
-    widthslider =
-        IntervalSlider(fig[2, 1], range = widthrange, startvalues = (1, framewidth))
-    heightslider = IntervalSlider(
-        fig[1, 2],
-        range = heightrange,
-        startvalues = (1, frameheight),
-        horizontal = false,
-    )
-    frameslider = SliderGrid(
+    w_sl = IntervalSlider(fig[2, 1], range = 1:w, startvalues = (1, w))
+    h_sl = IntervalSlider(fig[1, 2], range = 1:h, startvalues = (1, h), horizontal = false)
+    f_sl = SliderGrid(
         fig[3, :],
         (
             label = "Current\nframe",
-            range = 1:framecount,
+            range = 1:c,
             startvalue = 1,
             snap = false,
-            format = x -> "$x/$framecount",
+            format = x -> "$x/$c",
         ),
     )
-    textbox = Textbox(
+    range_tb = Textbox(
         fig[2, 3][1, 2],
         halign = :right,
         placeholder = "start frame, end frame",
@@ -57,20 +60,20 @@ function view_frames(
         width = 150,
     )
     lowerindex = Observable(1)
-    upperindex = Observable(framecount)
+    upperindex = Observable(c)
 
-    on(textbox.stored_string) do str
+    on(range_tb.stored_string) do str
         interval = sort!(parse.(Int64, split(str, ',')))
-        lowerindex[] = ifelse(1 <= interval[1] <= framecount, interval[1], lowerindex[])
-        upperindex[] = ifelse(1 <= interval[2] <= framecount, interval[2], upperindex[])
+        lowerindex[] = ifelse(1 <= interval[1] <= c, interval[1], lowerindex[])
+        upperindex[] = ifelse(1 <= interval[2] <= c, interval[2], upperindex[])
     end
 
-    bottomlefttag = @lift "($($(widthslider.interval)[1]), $($(heightslider.interval)[1]))"
-    toprighttag = @lift "($($(widthslider.interval)[2]), $($(heightslider.interval)[2]))"
+    sl_tags = (@lift "($($(w_sl.interval)[1]), $($(h_sl.interval)[1]))"),
+    (@lift "($($(w_sl.interval)[2]), $($(h_sl.interval)[2]))")
     rangetag =
         @lift "Frame range: $(min($lowerindex,$upperindex))-$(max($lowerindex,$upperindex))"
 
-    mainframe = @lift view(frames, :, :, $(frameslider.sliders[1].value))
+    mainframe = @lift view(frames, :, :, $(f_sl.sliders[1].value))
     startframe = @lift view(frames, :, :, $lowerindex)
     endframe = @lift view(frames, :, :, $upperindex)
 
@@ -83,31 +86,19 @@ function view_frames(
     #     @. (h_int[1] <= pointsx <= h_int[2]) & (v_int[1] <= pointsy <= v_int[2])
     # end
 
-    lbound = @lift $(widthslider.interval)[1] - 0.5
-    rbound = @lift $(widthslider.interval)[2] + 0.5
-    bbound = @lift $(heightslider.interval)[1] - 0.5
-    tbound = @lift $(heightslider.interval)[2] + 0.5
+    heatmap!(axes[1], 1:w, 1:h, mainframe, colormap = :bone)
+    heatmap!(axes[2], 1:w, 1:h, startframe, colormap = :bone)
+    heatmap!(axes[3], 1:w, 1:h, endframe, colormap = :bone)
+    limits!.(axes, 0.5, w + 0.5, 0.5, h + 0.5)
 
-    xmin = @lift ($(widthslider.interval)[1] - 1) / framewidth
-    xmax = @lift $(widthslider.interval)[2] / framewidth
-    ymin = @lift ($(heightslider.interval)[1] - 1) / framewidth
-    ymax = @lift $(heightslider.interval)[2] / framewidth
-
-    heatmap!(axes[1], widthrange, heightrange, mainframe, colormap = :bone)
-    heatmap!(axes[2], widthrange, heightrange, startframe, colormap = :bone)
-    heatmap!(axes[3], widthrange, heightrange, endframe, colormap = :bone)
-
-    limits!.(axes, 0.5, framewidth + 0.5, 0.5, frameheight + 0.5)
-
+    rect = getrect(w_sl, h_sl)
     for ax in axes
-        vlines!(ax, lbound, ymin = ymin, ymax = ymax, color = ColorSchemes.tab10[1])
-        vlines!(ax, rbound, ymin = ymin, ymax = ymax, color = ColorSchemes.tab10[1])
-        hlines!(ax, bbound, xmin = xmin, xmax = xmax, color = ColorSchemes.tab10[1])
-        hlines!(ax, tbound, xmin = xmin, xmax = xmax, color = ColorSchemes.tab10[1])
+        poly!(ax, rect, color = (:white, 0))
     end
 
-    text!(axes[1], lbound, bbound, align = (:left, :bottom), text = bottomlefttag)
-    text!(axes[1], rbound, tbound, align = (:right, :top), text = toprighttag)
+    verts = getvertices(rect)
+    text!(axes[1], verts[1], align = (:left, :bottom), text = sl_tags[1])
+    text!(axes[1], verts[2], align = (:right, :top), text = sl_tags[2])
 
     Label(fig[0, 1:2], "Frame viewer")
     Label(fig[0, 3], "Range viewer")
@@ -115,7 +106,7 @@ function view_frames(
 
     colsize!(fig.layout, 1, Relative(2 / 3))
     colsize!(fig.layout, 3, Relative(1 / 3))
-    rowsize!(fig.layout, 1, Aspect(1, frameheight / framewidth))
+    rowsize!(fig.layout, 1, Aspect(1, h / w))
     resize_to_layout!(fig)
     display(fig)
     set_theme!()
