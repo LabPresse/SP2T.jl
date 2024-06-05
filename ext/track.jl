f92754d5424dcc99672440591c7d88b1630de539
lanceXwq <lance.xwq@gmail.com>
2024-06-05 14:28:04 -0700

    using CUDA.@allowscalar for adding prior

--- ext/track.jl
+++ ext/track.jl
@@ -1,1 +1,12 @@
-SP2T.copyidxto!(x, xᵖ, idx::CuArray) = @. x = (idx * xᵖ) + (~idx * x)
+function SP2T.addΔlogπ₁!(
+    ln𝓇::CuArray{T},
+    x::AbstractArray{T,3},
+    y::AbstractArray{T,3},
+    prior::Normal₃,
+) where {T}
+    CUDA.@allowscalar ln𝓇[1] += SP2T.Δlog𝒫(x, y, prior)
+    return ln𝓇
+end
+
+SP2T.copyidxto!(x::AbstractArray{T}, y::AbstractArray{T}, idx::CuArray{Bool}) where {T} =
+    @. x = (idx * y) + (~idx * x)
