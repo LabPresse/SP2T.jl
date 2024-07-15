cac00f9c0bc7a02eb29c3cdfa4049290d631b844
lanceXwq <lance.xwq@gmail.com>
2024-07-15 10:40:59 -0700

    use Float32 by default

--- src/annealing.jl
+++ src/annealing.jl
@@ -2,7 +2,7 @@ abstract type AbstractAnnealing{T} end
 
 struct NoAnnealing{T} <: AbstractAnnealing{T} end
 
-NoAnnealing() = NoAnnealing{Int}()
+NoAnnealing() = NoAnnealing{Float32}()
 
 temperature(::NoAnnealing{T}, i) where {T} = one(T)
 
@@ -14,18 +14,9 @@ end
 
 PolynomialAnnealing{T}() where {T} = PolynomialAnnealing{T}(1, 1, 0)
 
-temperature(a::PolynomialAnnealing{T}, i) where {T} =
+temperature(a::PolynomialAnnealing{T}, i::Real) where {T} =
     i >= a.last_iter ? one(T) : a.init_temperature * (i / a.last_iter - oneunit(T))^a.order
 
 ftypof(::PolynomialAnnealing{T}) where {T} = T
 
-ftypof(::Nothing) = Nothing
-
-# PolynomialAnnealing{T}(
-#     init_temperature::Real = 1,
-#     cutoff_iteration::Real = 1,
-#     order::Integer = 2,
-# ) where {T<:AbstractFloat} = get_temperature(
-#     PolynomialAnnealing{T}(zero(T), init_temperature, cutoff_iteration, order),
-#     1,
-# )
+# ftypof(::Nothing) = Nothing
