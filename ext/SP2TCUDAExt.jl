6595453547bd097f553f8eeaf08d61caf0a7f0e1
lanceXwq <lance.xwq@gmail.com>
2024-08-06 14:47:43 -0700

    clean up SP2TCUDAExt

--- ext/SP2TCUDAExt.jl
+++ ext/SP2TCUDAExt.jl
@@ -1,9 +1,6 @@
 module SP2TCUDAExt
 
-using SP2T, CUDA, NNlib, LogExpFunctions, SpecialFunctions, Random, LinearAlgebra
-
-include("data.jl")
-include("chain.jl")
+using SP2T, CUDA
 
 include("nemitters.jl")
 include("track.jl")
