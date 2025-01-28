120201b41d3e59ec39242561736889cdb8ed8f72
Lance (Weiqing) Xu <lance.xwq@gmail.com>
2025-01-28 00:42:55 -0700

    remove legacy code

--- ext/SP2TCUDAExt.jl
+++ ext/SP2TCUDAExt.jl
@@ -2,7 +2,6 @@ module SP2TCUDAExt
 
 using SP2T, CUDA, Random
 
-# include("utils.jl")
 include("permutation.jl")
 include("track.jl")
 
--- ext/utils.jl
+++
@@ -1,3 +0,0 @@
-SP2T.unionalltypeof(::CuMatrix) = CuMatrix
-SP2T.unionalltypeof(::CuVector) = CuVector
-SP2T.unionalltypeof(::CuArray) = CuArray
