f32c8391c4bfa60677661ec693e8e2d19b5e33e6
lanceXwq <lance.xwq@gmail.com>
2025-05-15 08:27:47 -0700

    Only support inverse gamma as MSD's prior

--- src/msd.jl
+++ src/msd.jl
@@ -1,36 +1,50 @@
 """
     MeanSquaredDisplacement{T<:AbstractFloat, P} <: RandomVariable{T}
 
-A mutable struct representing the mean squared displacement random variable. `value::T` is the value, `prior::P` should be a probability distribution, `fixed::Bool` determines whether its `value` gets updated.
+A mutable struct representing the mean squared displacement random variable. `value::T` is the value, `prior::InverseGamma{T}` is an inverse gamma distrtibution, `fixed::Bool` determines whether its `value` gets updated.
 """
-mutable struct MeanSquaredDisplacement{T<:AbstractFloat,P} <: RandomVariable{T}
+mutable struct MeanSquaredDisplacement{T<:AbstractFloat} <: RandomVariable{T}
     value::T
-    prior::P
+    prior::InverseGamma{T}
     fixed::Bool
 end
 
+"""
+    MeanSquaredDisplacement{T}(; guess::Real, priorparams::Tuple{<:Real,<:Real}, fixed::Bool)
+
+The constructor for `MeanSquaredDisplacement`. By default, `priorparams = (2, 1e-5)' and `fixed = false`.
+"""
 MeanSquaredDisplacement{T}(;
     guess::Real,
-    prior::ContinuousUnivariateDistribution,
+    priorparams::Tuple{<:Real,<:Real} = (2, 1e-5),
     fixed::Bool = false,
 ) where {T<:AbstractFloat} = MeanSquaredDisplacement(
     convert(T, guess),
-    unionalltypeof(prior)(convert.(T, params(prior))...),
+    InverseGamma(convert.(Float32, priorparams)...),
     fixed,
 )
 
-logprior(msd::MeanSquaredDisplacement{T,P}) where {T,P<:InverseGamma{T}} =
+"""
+    logprior(msd::MeanSquaredDisplacement; normalization::Bool)
+
+Calculate the log density for `msd`'s prior given its current value. `normalization = false` by default. 
+"""
+logprior(msd::MeanSquaredDisplacement; normalization::Bool = false) =
+    normalization ? logpdf(msd.prior, msd.value) :
     -(shape(msd.prior) + 1) * log(msd.value) - scale(msd.prior) / msd.value
 
-logprior_norm(msd::MeanSquaredDisplacement{T,P}) where {T,P} = logpdf(msd.prior, msd.value)
+"""
+    update!(msd::MeanSquaredDisplacement{T}, displacement²::AbstractArray{T,3}, 𝑇::T)
 
+Directly sample `msd`'s new value from its posterior given the `displacement²` array and the temperature `𝑇`.
+"""
 function update!(
     msd::MeanSquaredDisplacement{T},
     displacement²::AbstractArray{T,3},
     𝑇::T,
 ) where {T}
     isfixed(msd) && return msd
-    Δparams = (length(displacement²), sum(vec(displacement²))) ./ (2 * 𝑇)
+    Δparams = (length(displacement²), sum(vec(displacement²))) ./ (2 * 𝑇) #* sum(vec(displacement²))) as summing vec is currently faster using CUDA.jl
     msd.value = _rand(msd.prior, Δparams)
     return msd
 end
